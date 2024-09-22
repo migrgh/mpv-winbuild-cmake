119fdb09101f313c7e060ac6bb3f17f5192c6346
migrgh <163189276+migrgh@users.noreply.github.com>
2024-09-22 15:25:06 +0200

    remove libmysofa

--- packages/ffmpeg.cmake
+++ packages/ffmpeg.cmake
@@ -14,7 +14,6 @@ ExternalProject_Add(ffmpeg
         libsoxr
         libvpx
         libwebp
-        libmysofa
         fontconfig
         harfbuzz
         opus
@@ -88,7 +87,7 @@ ExternalProject_Add(ffmpeg
         --disable-libzimg
         --enable-openssl
         --enable-libxml2
-        --enable-libmysofa
+        --disable-libmysofa
         --enable-libssh
         --enable-libsrt
         --enable-libvpl
