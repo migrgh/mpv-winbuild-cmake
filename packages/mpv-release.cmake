0bddf196a8cf0ed82e607e832598d4569b1b4b35
migrgh <163189276+migrgh@users.noreply.github.com>
2024-09-22 23:54:19 +0200

    proper disable pdf build

--- packages/mpv-release.cmake
+++ packages/mpv-release.cmake
@@ -84,7 +84,6 @@ ExternalProject_Add_Step(mpv-release copy-binary
     DEPENDEES strip-binary
     COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/mpv.exe                           ${CMAKE_CURRENT_BINARY_DIR}/mpv-package/mpv.exe
     COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/mpv.com                           ${CMAKE_CURRENT_BINARY_DIR}/mpv-package/mpv.com
-    COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/mpv.pdf                           ${CMAKE_CURRENT_BINARY_DIR}/mpv-package/doc/manual.pdf
     COMMAND ${CMAKE_COMMAND} -E copy ${MINGW_INSTALL_PREFIX}/etc/fonts/fonts.conf   ${CMAKE_CURRENT_BINARY_DIR}/mpv-package/mpv/fonts.conf
     COMMENT "Copying mpv binaries and manual"
 )
